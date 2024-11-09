84d5f3ef27e45eeff0363efaa85c95bf5da9bb7b
pedrosferreira <ferreirapedro.sjm@gmail.com>
2024-11-09 16:59:19 +0000

    Initial commit

---
+++ src/main/java/com/codechallenge/speed_metrics/SpeedMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.speed_metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codechallenge/speed_metrics/SpeedMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.speed_metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
